51d3c8213cb051cbada3e08817c60653380d7664
enriquefdez5 <uo257742@uniovi.es>
2019-02-22 09:21:11 +0100

    SDI-SpringBoot-Sesión-4.4-Consulta

--- sdi1102-lab-spring/src/main/java/com/uniovi/controllers/MarksControllers.java
+++ sdi1102-lab-spring/src/main/java/com/uniovi/controllers/MarksControllers.java
@@ -1,7 +1,6 @@
 package com.uniovi.controllers;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.security.Principal;
 
 import javax.servlet.http.HttpSession;
 
@@ -16,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.uniovi.entities.Mark;
+import com.uniovi.entities.User;
 import com.uniovi.services.MarksService;
 import com.uniovi.services.UsersService;
 import com.uniovi.validators.AddMarkValidator;
@@ -34,11 +34,33 @@ public class MarksControllers {
 
 	@Autowired
 	private AddMarkValidator addMarkValidator;
+//
+//	@RequestMapping("/mark/list")
+//	public String getList(Model model) {
+//		model.addAttribute("markList", marksService.getMarks());
+//		return "mark/list";
+//	}
 
 	@RequestMapping("/mark/list")
-	public String getList(Model model) {
-		model.addAttribute("markList", marksService.getMarks());
+	public String getList(Model model, Principal principal) {
+		String dni = principal.getName(); // DNI es el name de la autenticación
+		User user = usersService.getUserByDni(dni);
+		model.addAttribute("markList", marksService.getMarksForUser(user));
 		return "mark/list";
+	}
+
+//	@RequestMapping("/mark/list/update")
+//	public String updateList(Model model) {
+//		model.addAttribute("markList", marksService.getMarks());
+//		return "mark/list :: tableMarks";
+//	}
+
+	@RequestMapping("/mark/list/update")
+	public String updateList(Model model, Principal principal) {
+		String dni = principal.getName(); // DNI es el name de la autenticación
+		User user = usersService.getUserByDni(dni);
+		model.addAttribute("markList", marksService.getMarksForUser(user));
+		return "mark/list :: tableMarks";
 	}
 
 	@RequestMapping(value = "/mark/add", method = RequestMethod.POST)
@@ -87,12 +109,6 @@ public class MarksControllers {
 		original.setDescription(mark.getDescription());
 		marksService.addMark(original);
 		return "redirect:/mark/details/" + id;
-	}
-
-	@RequestMapping("/mark/list/update")
-	public String updateList(Model model) {
-		model.addAttribute("markList", marksService.getMarks());
-		return "mark/list :: tableMarks";
 	}
 
 	@RequestMapping(value = "/mark/{id}/resend", method = RequestMethod.GET)
